455b823431f99aae5b01bd15514aa0e6167c92c0
Kristen Leach <kristenleach24@gmail.com>
2017-11-26 06:07:03 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,9 @@
 def fizzbuzz(i)
   if i % 3 == 0 && i % 5 == 0
     puts "fizzbuzz"
-  end
-  elsif i % 3 == 0
+  end elsif i % 3 == 0
     puts "Fizz"
-  end
-  elsif i % 5 == 0
+  end elsif i % 5 == 0
     puts "Buzz"
   end
 end 
